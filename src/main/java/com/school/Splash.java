053afb519d54e4718b9afafd8aa507e943286cca
vaibhavvev12311 <89141278+vaibhavvev12311@users.noreply.github.com>
2021-08-20 15:35:36 +0530

    Add files via upload

---
+++ src/main/java/com/school/Splash.java
@@ -0,0 +1,54 @@
+package com.school;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Splash {
+
+	public static void main(String s[]) {
+		Frame f = new Frame("Amazon School");
+		f.setVisible(true);
+		int i;
+		int x=1;
+		for(i=2;i<=600;i+=4,x+=1) {
+			f.setLocation((800-((i+x)/2)),500-(i/2));
+			f.setSize(i+3*x,i+x/2);
+			
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		
+	}
+}
+
+class Frame extends JFrame implements Runnable{
+	Thread t1;
+	Frame(String s){
+		super(s);
+		
+			setLayout(new FlowLayout());
+			ImageIcon c1 = new ImageIcon(ClassLoader.getSystemResource("com/school/icons/first.jpg"));
+			Image i1 = c1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
+			ImageIcon i2 =  new ImageIcon(i1);
+			
+			JLabel m1 = new JLabel(i2);
+			add(m1);
+			t1 = new Thread(this);
+			t1.start();
+			
+		}
+	public void run() {
+		try {
+			Thread.sleep(7000);
+			this.setVisible(false);
+			Login f1 = new Login();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	}
---
+++ src/main/java/com/school/conn.java
@@ -0,0 +1,20 @@
+package com.school;
+
+import java.sql.*;
+
+
+
+public class conn {
+
+	Connection c;
+	Statement s;
+	public conn() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			c=DriverManager.getConnection("jdbc:mysql://localhost:3306/school", "root", "Vev@452006");
+			s=c.createStatement();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
